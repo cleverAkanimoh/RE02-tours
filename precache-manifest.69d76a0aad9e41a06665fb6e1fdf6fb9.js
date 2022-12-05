fd050fb8577d42523ddd00c47062b2737d462a3f
Clever AKANIMOH <crushclever1@gmail.com>
2022-12-05 09:05:27 +0100

    Add files via upload

---
+++ precache-manifest.69d76a0aad9e41a06665fb6e1fdf6fb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b8ca4bb71c47d0605483dbf16a4714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50149c74577de2ad6f8c",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "367d3ad82c25432129b5",
+    "url": "/static/js/2.0bb548be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0bb548be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50149c74577de2ad6f8c",
+    "url": "/static/js/main.e7a74f05.chunk.js"
+  },
+  {
+    "revision": "9221554562899147aff5",
+    "url": "/static/js/runtime-main.ba293752.js"
+  }
+]);
